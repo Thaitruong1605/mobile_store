eadd6d94826d9a3b61006f31aec976c66fa61168
thaitruong1605 <thaitruong1605@gmail.com>
2021-04-10 21:23:56 +0700

    update params

--- params/params.js
+++ params/params.js
@@ -1,4 +1,4 @@
-const DATABASECONNECTION = "mongodb+srv://thaitruong:7aPS.uv8qzfdB9@@cluster0.wlb4g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const DATABASECONNECTION = "mongodb+srv://thaitruong:5z6MTCqCSb3SiIxes@cluster0.wlb4g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
 module.exports = {
     DATABASECONNECTION 
